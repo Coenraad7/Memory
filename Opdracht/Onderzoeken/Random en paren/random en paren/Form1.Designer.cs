1319d62e5532cb9142cda15913cc3dd162a9dd00
Richard 1G <jong1737@student.nhl.nl>
2017-10-17 13:23:48 +0200

    Version 0.90

--- Opdracht/Onderzoeken/Random en paren/random en paren/Form1.Designer.cs
+++ Opdracht/Onderzoeken/Random en paren/random en paren/Form1.Designer.cs
@@ -28,12 +28,229 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.kaart1 = new System.Windows.Forms.Button();
+            this.kaart2 = new System.Windows.Forms.Button();
+            this.kaart3 = new System.Windows.Forms.Button();
+            this.kaart4 = new System.Windows.Forms.Button();
+            this.kaart5 = new System.Windows.Forms.Button();
+            this.kaart6 = new System.Windows.Forms.Button();
+            this.kaart7 = new System.Windows.Forms.Button();
+            this.kaart8 = new System.Windows.Forms.Button();
+            this.kaart9 = new System.Windows.Forms.Button();
+            this.kaart10 = new System.Windows.Forms.Button();
+            this.kaart11 = new System.Windows.Forms.Button();
+            this.kaart12 = new System.Windows.Forms.Button();
+            this.kaart13 = new System.Windows.Forms.Button();
+            this.kaart14 = new System.Windows.Forms.Button();
+            this.kaart15 = new System.Windows.Forms.Button();
+            this.kaart16 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // kaart1
+            // 
+            this.kaart1.Location = new System.Drawing.Point(164, 75);
+            this.kaart1.Name = "kaart1";
+            this.kaart1.Size = new System.Drawing.Size(100, 100);
+            this.kaart1.TabIndex = 0;
+            this.kaart1.Text = "kaart1";
+            this.kaart1.UseVisualStyleBackColor = true;
+            this.kaart1.Click += new System.EventHandler(this.kaart1_Click);
+            // 
+            // kaart2
+            // 
+            this.kaart2.Location = new System.Drawing.Point(270, 75);
+            this.kaart2.Name = "kaart2";
+            this.kaart2.Size = new System.Drawing.Size(100, 100);
+            this.kaart2.TabIndex = 1;
+            this.kaart2.Text = "kaart2";
+            this.kaart2.UseVisualStyleBackColor = true;
+            this.kaart2.Click += new System.EventHandler(this.kaart2_Click);
+            // 
+            // kaart3
+            // 
+            this.kaart3.Location = new System.Drawing.Point(376, 75);
+            this.kaart3.Name = "kaart3";
+            this.kaart3.Size = new System.Drawing.Size(100, 100);
+            this.kaart3.TabIndex = 3;
+            this.kaart3.Text = "kaart3";
+            this.kaart3.UseVisualStyleBackColor = true;
+            this.kaart3.Click += new System.EventHandler(this.kaart3_Click);
+            // 
+            // kaart4
+            // 
+            this.kaart4.Location = new System.Drawing.Point(482, 75);
+            this.kaart4.Name = "kaart4";
+            this.kaart4.Size = new System.Drawing.Size(100, 100);
+            this.kaart4.TabIndex = 2;
+            this.kaart4.Text = "kaart4";
+            this.kaart4.UseVisualStyleBackColor = true;
+            this.kaart4.Click += new System.EventHandler(this.kaart4_Click);
+            // 
+            // kaart5
+            // 
+            this.kaart5.Location = new System.Drawing.Point(164, 181);
+            this.kaart5.Name = "kaart5";
+            this.kaart5.Size = new System.Drawing.Size(100, 100);
+            this.kaart5.TabIndex = 7;
+            this.kaart5.Text = "kaart5";
+            this.kaart5.UseVisualStyleBackColor = true;
+            this.kaart5.Click += new System.EventHandler(this.kaart5_Click);
+            // 
+            // kaart6
+            // 
+            this.kaart6.Location = new System.Drawing.Point(270, 181);
+            this.kaart6.Name = "kaart6";
+            this.kaart6.Size = new System.Drawing.Size(100, 100);
+            this.kaart6.TabIndex = 6;
+            this.kaart6.Text = "kaart6";
+            this.kaart6.UseVisualStyleBackColor = true;
+            this.kaart6.Click += new System.EventHandler(this.kaart6_Click);
+            // 
+            // kaart7
+            // 
+            this.kaart7.Location = new System.Drawing.Point(376, 181);
+            this.kaart7.Name = "kaart7";
+            this.kaart7.Size = new System.Drawing.Size(100, 100);
+            this.kaart7.TabIndex = 5;
+            this.kaart7.Text = "kaart7";
+            this.kaart7.UseVisualStyleBackColor = true;
+            this.kaart7.Click += new System.EventHandler(this.kaart7_Click);
+            // 
+            // kaart8
+            // 
+            this.kaart8.Location = new System.Drawing.Point(482, 181);
+            this.kaart8.Name = "kaart8";
+            this.kaart8.Size = new System.Drawing.Size(100, 100);
+            this.kaart8.TabIndex = 4;
+            this.kaart8.Text = "kaart8";
+            this.kaart8.UseVisualStyleBackColor = true;
+            this.kaart8.Click += new System.EventHandler(this.kaart8_Click);
+            // 
+            // kaart9
+            // 
+            this.kaart9.Location = new System.Drawing.Point(164, 287);
+            this.kaart9.Name = "kaart9";
+            this.kaart9.Size = new System.Drawing.Size(100, 100);
+            this.kaart9.TabIndex = 11;
+            this.kaart9.Text = "kaart9";
+            this.kaart9.UseVisualStyleBackColor = true;
+            this.kaart9.Click += new System.EventHandler(this.kaart9_Click);
+            // 
+            // kaart10
+            // 
+            this.kaart10.Location = new System.Drawing.Point(270, 287);
+            this.kaart10.Name = "kaart10";
+            this.kaart10.Size = new System.Drawing.Size(100, 100);
+            this.kaart10.TabIndex = 10;
+            this.kaart10.Text = "kaart10";
+            this.kaart10.UseVisualStyleBackColor = true;
+            this.kaart10.Click += new System.EventHandler(this.kaart10_Click);
+            // 
+            // kaart11
+            // 
+            this.kaart11.Location = new System.Drawing.Point(376, 287);
+            this.kaart11.Name = "kaart11";
+            this.kaart11.Size = new System.Drawing.Size(100, 100);
+            this.kaart11.TabIndex = 9;
+            this.kaart11.Text = "kaart11";
+            this.kaart11.UseVisualStyleBackColor = true;
+            this.kaart11.Click += new System.EventHandler(this.kaart11_Click);
+            // 
+            // kaart12
+            // 
+            this.kaart12.Location = new System.Drawing.Point(482, 287);
+            this.kaart12.Name = "kaart12";
+            this.kaart12.Size = new System.Drawing.Size(100, 100);
+            this.kaart12.TabIndex = 8;
+            this.kaart12.Text = "kaart12";
+            this.kaart12.UseVisualStyleBackColor = true;
+            this.kaart12.Click += new System.EventHandler(this.kaart12_Click);
+            // 
+            // kaart13
+            // 
+            this.kaart13.Location = new System.Drawing.Point(164, 393);
+            this.kaart13.Name = "kaart13";
+            this.kaart13.Size = new System.Drawing.Size(100, 100);
+            this.kaart13.TabIndex = 15;
+            this.kaart13.Text = "kaart13";
+            this.kaart13.UseVisualStyleBackColor = true;
+            this.kaart13.Click += new System.EventHandler(this.kaart13_Click);
+            // 
+            // kaart14
+            // 
+            this.kaart14.Location = new System.Drawing.Point(270, 393);
+            this.kaart14.Name = "kaart14";
+            this.kaart14.Size = new System.Drawing.Size(100, 100);
+            this.kaart14.TabIndex = 14;
+            this.kaart14.Text = "kaart14";
+            this.kaart14.UseVisualStyleBackColor = true;
+            this.kaart14.Click += new System.EventHandler(this.kaart14_Click);
+            // 
+            // kaart15
+            // 
+            this.kaart15.Location = new System.Drawing.Point(376, 393);
+            this.kaart15.Name = "kaart15";
+            this.kaart15.Size = new System.Drawing.Size(100, 100);
+            this.kaart15.TabIndex = 13;
+            this.kaart15.Text = "kaart15";
+            this.kaart15.UseVisualStyleBackColor = true;
+            this.kaart15.Click += new System.EventHandler(this.kaart15_Click);
+            // 
+            // kaart16
+            // 
+            this.kaart16.Location = new System.Drawing.Point(482, 393);
+            this.kaart16.Name = "kaart16";
+            this.kaart16.Size = new System.Drawing.Size(100, 100);
+            this.kaart16.TabIndex = 12;
+            this.kaart16.Text = "kaart16";
+            this.kaart16.UseVisualStyleBackColor = true;
+            this.kaart16.Click += new System.EventHandler(this.kaart16_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 586);
+            this.Controls.Add(this.kaart13);
+            this.Controls.Add(this.kaart14);
+            this.Controls.Add(this.kaart15);
+            this.Controls.Add(this.kaart16);
+            this.Controls.Add(this.kaart9);
+            this.Controls.Add(this.kaart10);
+            this.Controls.Add(this.kaart11);
+            this.Controls.Add(this.kaart12);
+            this.Controls.Add(this.kaart5);
+            this.Controls.Add(this.kaart6);
+            this.Controls.Add(this.kaart7);
+            this.Controls.Add(this.kaart8);
+            this.Controls.Add(this.kaart3);
+            this.Controls.Add(this.kaart4);
+            this.Controls.Add(this.kaart2);
+            this.Controls.Add(this.kaart1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button kaart1;
+        private System.Windows.Forms.Button kaart2;
+        private System.Windows.Forms.Button kaart3;
+        private System.Windows.Forms.Button kaart4;
+        private System.Windows.Forms.Button kaart5;
+        private System.Windows.Forms.Button kaart6;
+        private System.Windows.Forms.Button kaart7;
+        private System.Windows.Forms.Button kaart8;
+        private System.Windows.Forms.Button kaart9;
+        private System.Windows.Forms.Button kaart10;
+        private System.Windows.Forms.Button kaart11;
+        private System.Windows.Forms.Button kaart12;
+        private System.Windows.Forms.Button kaart13;
+        private System.Windows.Forms.Button kaart14;
+        private System.Windows.Forms.Button kaart15;
+        private System.Windows.Forms.Button kaart16;
     }
 }
 
--- Opdracht/Onderzoeken/Random en paren/random en paren/Form1.cs
+++ Opdracht/Onderzoeken/Random en paren/random en paren/Form1.cs
@@ -16,5 +16,89 @@ namespace random_en_paren
         {
             InitializeComponent();
         }
+
+        private void kaart1_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            button.BackColor = Color.Turquoise;
+            button.Visible = false;
+            button.Visible = true;
+        }
+
+        private void kaart2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kaart16_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

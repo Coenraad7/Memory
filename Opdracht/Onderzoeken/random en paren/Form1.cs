6355c059218f665c859f4e684f60b26e13cf8f5e
Richard 1G <jong1737@student.nhl.nl>
2017-10-18 15:31:53 +0200

    Version 0.93

--- Opdracht/Onderzoeken/random en paren/Form1.cs
+++ Opdracht/Onderzoeken/random en paren/Form1.cs
@@ -12,15 +12,17 @@ namespace random_en_paren
 {
     public partial class Form1 : Form
     {
-        int[,] cardproperties = new int[36, 2];
+        int[,] cardproperties;
         int count,card1,card2,oldcard = -1,oldcard2 = -1;
         PictureBox cardinfo1,cardinfo2;
 
         public Form1()
         {
+            int amount = 16; //aantal kaarten die gebruikt worden
+            cardproperties = new int[amount, 2]; //1e getal is kaartnummer/picturebox, 2e getal is een property ,1 is bvb paarnummer(bij 16 kaarten 1tm8)
             InitializeComponent();
             label1.Text = "";
-            scramble();
+            scramble(); //spel begin kaarten randomizen
         }
 
         private void scramble()
@@ -29,7 +31,7 @@ namespace random_en_paren
             bool done = false, check = false;
             int random = 0;
 
-            for (int i = 0; i < 16; i++)
+            for (int i = 0; i < cardproperties.GetLength(0); i++)
             {
                 done = false;
                 check = false;
@@ -38,48 +40,44 @@ namespace random_en_paren
                 {
                     check = true;
                     done = true;
-                    random = rng.Next(1, 9);
-                    for (int e = 0; e < 16; e++)
+                    random = rng.Next(1, (cardproperties.GetLength(0)/2+1));
+                    for (int e = 0; e < cardproperties.GetLength(0); e++)
                     {
                         
-                        if ((cardproperties[e, 1] == random) && (check == false))
+                        if ((cardproperties[e, 1] == random) && (check == false)) //2e check dat er een nummer 2x instaat (dit mag niet)
                         {
                             done = false;
                         }
-                        else if ((cardproperties[e, 1] == random) && (check == true))
+                        else if ((cardproperties[e, 1] == random) && (check == true)) //1e check dat er 1 nummer instaat (dit mag wel)
                         {
                             check = false;
                         }
                     }                   
                 }
-                cardproperties[i, 1] = random;
-                //label1.Text += Convert.ToString(cardproperties[i, 1]) + ",";
+                cardproperties[i, 1] = random; //nummer aan de array geven
             }
         }
 
         private void rotate(object sender, int picture)
         {
             PictureBox card = (PictureBox)sender;
-            card.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("_" + cardproperties[picture,1]);
-            //label1.Text = Convert.ToString (cardproperties[picture, 1]);
+            card.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject("_" + cardproperties[picture,1]); //kaartje naar juiste backgroundimage zetten (afhankelijk van paarnummer)
 
-            if (card1 == 0)
+            if (card1 == 0) //1e klik (word maar 1x gedraaid in de code)
             {
                 card1 = cardproperties[picture, 1];
                 cardinfo1 = card;
                 oldcard = picture;
             }
-            else if (card2 == 0 && oldcard != picture)
+            else if (card2 == 0 && oldcard != picture) //2e klik
             {
-                //label1.Text = oldcard + "," + picture;
                 card2 = cardproperties[picture, 1];
                 cardinfo2 = card;
                 oldcard2 = picture;
-                paircheck();
+                paircheck(); //hier word de methode voor het controlleren van paren opgevraagd
             }
-            else if (card2 != 0 && oldcard != picture && oldcard2 != picture)
+            else if (card2 != 0 && oldcard != picture && oldcard2 != picture) //3e klik en ook 1e (omdat de 3e klik ook een kaartje registreerd)
             {
-                //label1.Text = oldcard + "," + picture;
                 cardinfo1.BackgroundImage = Properties.Resources._default;
                 cardinfo2.BackgroundImage = Properties.Resources._default;
                 card1 = cardproperties[picture, 1];
@@ -92,8 +90,7 @@ namespace random_en_paren
 
         private void paircheck()
         {
-            //label1.Text = card1 + "," + card2;
-            if (card1 == card2)
+            if (card1 == card2) //als de waarden gelijk zijn zal hij ze terug zetten naar standaard(_default) en verstoppen
             {
                 cardinfo1.BackgroundImage = Properties.Resources._default;
                 cardinfo2.BackgroundImage = Properties.Resources._default;
@@ -108,26 +105,31 @@ namespace random_en_paren
 
         private void Reset()
         {
-            foreach (var pictureBox in Controls.OfType<PictureBox>())
+            foreach (var pictureBox in Controls.OfType<PictureBox>()) //vraagt alle picture boxes op
             {
-                //int imageIndex = (int)pictureBox.Tag;
-                pictureBox.BackgroundImage = Properties.Resources._default;
-                pictureBox.Visible = true;
+                for (int i = 1; i <= cardproperties.GetLength(0); i++) 
+                {
+                    if (pictureBox.Name == ("pictureBox" + Convert.ToString(i))) //vergelijkt de namen van de pictureboxes die gebruikt worden en reset ze naar _default en visible
+                    {
+                        pictureBox.BackgroundImage = Properties.Resources._default;
+                        pictureBox.Visible = true;
+                    }
+                }
             }
-            card1 = 0;
+            card1 = 0; //alle code die hier staat zorgt er voor dat de standaard waarden terug worden gezet
             cardinfo1 = null;
             card2 = 0;
             cardinfo2 = null;
-            for (int r = 0; r < 16; r++)
+            for (int r = 0; r < cardproperties.GetLength(0); r++)
             {
                 cardproperties[r, 1] = 0;
             }
-            scramble();
+            scramble(); //vraagt scramble op om zo de kaartjes te husselen
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            rotate(sender, 0);
+            rotate(sender, 0); //rotate is een methode voor het omdraaien van de kaartjes, sender geeft de informatie mee van de picturebox, 0 geeft aan dat hij slot 0 gebruikt in de array (cardproperties[0,*])
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)

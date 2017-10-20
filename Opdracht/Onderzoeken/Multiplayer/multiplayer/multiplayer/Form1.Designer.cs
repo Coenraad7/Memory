0530a71c7a3698c6edf7b213af6b88deb31c0d3d
”dantheman_0125” <danielroskam@gmail.com>
2017-10-20 14:11:57 +0200

    version 1.02

---
+++ Opdracht/Onderzoeken/Multiplayer/multiplayer/multiplayer/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace multiplayer
+{
+    partial class multiplayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.player1 = new System.Windows.Forms.RadioButton();
+            this.player2 = new System.Windows.Forms.RadioButton();
+            this.player4 = new System.Windows.Forms.RadioButton();
+            this.player3 = new System.Windows.Forms.RadioButton();
+            this.p1box = new System.Windows.Forms.TextBox();
+            this.p2box = new System.Windows.Forms.TextBox();
+            this.p3box = new System.Windows.Forms.TextBox();
+            this.p4box = new System.Windows.Forms.TextBox();
+            this.p1txt = new System.Windows.Forms.Label();
+            this.save = new System.Windows.Forms.Button();
+            this.p2txt = new System.Windows.Forms.Label();
+            this.p3txt = new System.Windows.Forms.Label();
+            this.p4txt = new System.Windows.Forms.Label();
+            this.mis1 = new System.Windows.Forms.Label();
+            this.mis2 = new System.Windows.Forms.Label();
+            this.mis3 = new System.Windows.Forms.Label();
+            this.mis4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // player1
+            // 
+            this.player1.AutoSize = true;
+            this.player1.Location = new System.Drawing.Point(29, 14);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(82, 17);
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = true;
+            this.player1.Text = "Singleplayer";
+            this.player1.UseVisualStyleBackColor = true;
+            this.player1.CheckedChanged += new System.EventHandler(this.player1_CheckedChanged);
+            // 
+            // player2
+            // 
+            this.player2.AutoSize = true;
+            this.player2.Location = new System.Drawing.Point(29, 43);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(68, 17);
+            this.player2.TabIndex = 1;
+            this.player2.TabStop = true;
+            this.player2.Text = "2 Players";
+            this.player2.UseVisualStyleBackColor = true;
+            this.player2.CheckedChanged += new System.EventHandler(this.player2_CheckedChanged);
+            // 
+            // player4
+            // 
+            this.player4.AutoSize = true;
+            this.player4.Location = new System.Drawing.Point(29, 92);
+            this.player4.Name = "player4";
+            this.player4.Size = new System.Drawing.Size(68, 17);
+            this.player4.TabIndex = 3;
+            this.player4.TabStop = true;
+            this.player4.Text = "4 Players";
+            this.player4.UseVisualStyleBackColor = true;
+            this.player4.CheckedChanged += new System.EventHandler(this.player4_CheckedChanged);
+            // 
+            // player3
+            // 
+            this.player3.AutoSize = true;
+            this.player3.Location = new System.Drawing.Point(29, 66);
+            this.player3.Name = "player3";
+            this.player3.Size = new System.Drawing.Size(68, 17);
+            this.player3.TabIndex = 2;
+            this.player3.TabStop = true;
+            this.player3.Text = "3 Players";
+            this.player3.UseVisualStyleBackColor = true;
+            this.player3.CheckedChanged += new System.EventHandler(this.player3_CheckedChanged);
+            // 
+            // p1box
+            // 
+            this.p1box.Location = new System.Drawing.Point(141, 13);
+            this.p1box.Name = "p1box";
+            this.p1box.Size = new System.Drawing.Size(100, 20);
+            this.p1box.TabIndex = 4;
+            // 
+            // p2box
+            // 
+            this.p2box.Location = new System.Drawing.Point(141, 39);
+            this.p2box.Name = "p2box";
+            this.p2box.Size = new System.Drawing.Size(100, 20);
+            this.p2box.TabIndex = 5;
+            this.p2box.Visible = false;
+            // 
+            // p3box
+            // 
+            this.p3box.Location = new System.Drawing.Point(141, 65);
+            this.p3box.Name = "p3box";
+            this.p3box.Size = new System.Drawing.Size(100, 20);
+            this.p3box.TabIndex = 6;
+            this.p3box.Visible = false;
+            // 
+            // p4box
+            // 
+            this.p4box.Location = new System.Drawing.Point(141, 91);
+            this.p4box.Name = "p4box";
+            this.p4box.Size = new System.Drawing.Size(100, 20);
+            this.p4box.TabIndex = 7;
+            this.p4box.Visible = false;
+            // 
+            // p1txt
+            // 
+            this.p1txt.AutoSize = true;
+            this.p1txt.Location = new System.Drawing.Point(44, 210);
+            this.p1txt.Name = "p1txt";
+            this.p1txt.Size = new System.Drawing.Size(0, 13);
+            this.p1txt.TabIndex = 9;
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(141, 129);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(100, 23);
+            this.save.TabIndex = 10;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // p2txt
+            // 
+            this.p2txt.AutoSize = true;
+            this.p2txt.Location = new System.Drawing.Point(129, 249);
+            this.p2txt.Name = "p2txt";
+            this.p2txt.Size = new System.Drawing.Size(0, 13);
+            this.p2txt.TabIndex = 9;
+            // 
+            // p3txt
+            // 
+            this.p3txt.AutoSize = true;
+            this.p3txt.Location = new System.Drawing.Point(244, 210);
+            this.p3txt.Name = "p3txt";
+            this.p3txt.Size = new System.Drawing.Size(0, 13);
+            this.p3txt.TabIndex = 9;
+            // 
+            // p4txt
+            // 
+            this.p4txt.AutoSize = true;
+            this.p4txt.Location = new System.Drawing.Point(370, 249);
+            this.p4txt.Name = "p4txt";
+            this.p4txt.Size = new System.Drawing.Size(0, 13);
+            this.p4txt.TabIndex = 9;
+            // 
+            // mis1
+            // 
+            this.mis1.AutoSize = true;
+            this.mis1.Location = new System.Drawing.Point(260, 16);
+            this.mis1.Name = "mis1";
+            this.mis1.Size = new System.Drawing.Size(120, 13);
+            this.mis1.TabIndex = 12;
+            this.mis1.Text = "<-- Insert your nickname";
+            this.mis1.Visible = false;
+            // 
+            // mis2
+            // 
+            this.mis2.AutoSize = true;
+            this.mis2.Location = new System.Drawing.Point(260, 42);
+            this.mis2.Name = "mis2";
+            this.mis2.Size = new System.Drawing.Size(120, 13);
+            this.mis2.TabIndex = 12;
+            this.mis2.Text = "<-- Insert your nickname";
+            this.mis2.Visible = false;
+            // 
+            // mis3
+            // 
+            this.mis3.AutoSize = true;
+            this.mis3.Location = new System.Drawing.Point(260, 68);
+            this.mis3.Name = "mis3";
+            this.mis3.Size = new System.Drawing.Size(120, 13);
+            this.mis3.TabIndex = 12;
+            this.mis3.Text = "<-- Insert your nickname";
+            this.mis3.Visible = false;
+            // 
+            // mis4
+            // 
+            this.mis4.AutoSize = true;
+            this.mis4.Location = new System.Drawing.Point(260, 94);
+            this.mis4.Name = "mis4";
+            this.mis4.Size = new System.Drawing.Size(120, 13);
+            this.mis4.TabIndex = 12;
+            this.mis4.Text = "<-- Insert your nickname";
+            this.mis4.Visible = false;
+            // 
+            // multiplayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 385);
+            this.Controls.Add(this.mis4);
+            this.Controls.Add(this.mis3);
+            this.Controls.Add(this.mis2);
+            this.Controls.Add(this.mis1);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.p4txt);
+            this.Controls.Add(this.p3txt);
+            this.Controls.Add(this.p2txt);
+            this.Controls.Add(this.p1txt);
+            this.Controls.Add(this.p3box);
+            this.Controls.Add(this.p4box);
+            this.Controls.Add(this.p2box);
+            this.Controls.Add(this.p1box);
+            this.Controls.Add(this.player4);
+            this.Controls.Add(this.player3);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player1);
+            this.Name = "multiplayer";
+            this.Text = "Multiplayer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton player1;
+        private System.Windows.Forms.RadioButton player2;
+        private System.Windows.Forms.RadioButton player4;
+        private System.Windows.Forms.RadioButton player3;
+        private System.Windows.Forms.TextBox p1box;
+        private System.Windows.Forms.TextBox p2box;
+        private System.Windows.Forms.TextBox p3box;
+        private System.Windows.Forms.TextBox p4box;
+        private System.Windows.Forms.Label p1txt;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Label p2txt;
+        private System.Windows.Forms.Label p3txt;
+        private System.Windows.Forms.Label p4txt;
+        private System.Windows.Forms.Label mis1;
+        private System.Windows.Forms.Label mis2;
+        private System.Windows.Forms.Label mis3;
+        private System.Windows.Forms.Label mis4;
+    }
+}
+
---
+++ Opdracht/Onderzoeken/Multiplayer/multiplayer/multiplayer/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multiplayer
+{
+    public partial class multiplayer : Form
+    {
+
+        public multiplayer()
+        {
+            InitializeComponent();
+        }
+
+        private void player1_CheckedChanged(object sender, EventArgs e)
+        {
+            p2box.Visible = false;
+            p3box.Visible = false;
+            p4box.Visible = false;
+        }
+
+        private void player2_CheckedChanged(object sender, EventArgs e)
+        {
+            p2box.Visible = true;
+            p3box.Visible = false;
+            p4box.Visible = false;
+        }
+
+        private void player3_CheckedChanged(object sender, EventArgs e)
+        {
+            p2box.Visible = true;
+            p3box.Visible = true;
+            p4box.Visible = false;
+        }
+
+        private void player4_CheckedChanged(object sender, EventArgs e)
+        {
+            p2box.Visible = true;
+            p3box.Visible = true;
+            p4box.Visible = true;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            mis3.Visible = false;
+            mis4.Visible = false;
+
+            if (player1.Visible == true && p1box.Text == "")
+            {
+                mis1.Visible = true;
+            }
+            else
+            {
+                mis1.Visible = false;
+            }
+            if (player2.Visible == true && p2box.Text == "")
+            {
+                mis2.Visible = true;
+            }
+            else
+            {
+                mis2.Visible = false;
+            }
+
+            if (player3.Visible == true && p3box.Text == "")
+            {
+                mis3.Visible = true;
+            }
+            else
+            {
+                p1txt.Text = p1box.Text;
+                p2txt.Text = p2box.Text;
+                p3txt.Text = p3box.Text;
+                p4txt.Text = p4box.Text;
+            }
+        }
+    }
+}

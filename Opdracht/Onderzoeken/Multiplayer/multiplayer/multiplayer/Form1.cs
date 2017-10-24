ecfb5336cc386e3060a283e691b22e6fa08ab756
”dantheman_0125” <danielroskam@gmail.com>
2017-10-24 20:56:51 +0200

    version 1.13

--- Opdracht/Onderzoeken/Multiplayer/multiplayer/multiplayer/Form1.cs
+++ Opdracht/Onderzoeken/Multiplayer/multiplayer/multiplayer/Form1.cs
@@ -49,34 +49,78 @@ namespace multiplayer
         private void save_Click(object sender, EventArgs e)
         {
 
-            if (player1.Visible == true && p1box.Text == "")
-            {
-                
-            }
-            else
+            if (!(p1box.Text == "") && (p2box.Visible == true && !(p2box.Text == "") || p2box.Visible == false) && (p3box.Visible == true && !(p3box.Text == "") || p3box.Visible == false) && (p4box.Visible == true && !(p4box.Text == "") || p4box.Visible == false)) && ((dif1.Checked) || (dif2.Checked) || (dif3.Checked) || (dif4.Checked) || (dif5.Checked)) && ((theme1.Checked) || (theme2.Checked) || (theme3.Checked) || (theme4.Checked) || (theme5.Checked))
             {
 
+                {
+
+                    if (p1box.Visible == true && p1box.Text == "")
+                    {
+                        p1box.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        p1txt.Text = p1box.Text;
+                    }
+                    if (p2box.Visible == true && p2box.Text == "")
+                    {
+                        p2box.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        p2txt.Text = p2box.Text;
+                    }
+
+                    if (p3box.Visible == true && p3box.Text == "")
+                    {
+                        p3box.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        p3txt.Text = p3box.Text;
+                    }
+
+                    if (p4box.Visible && p4box.Text == "")
+                    {
+                        p4box.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        p4txt.Text = p4box.Text;
+                    }
+                }
+
+                {
+                    MessageBox.Show("alles is goed");
+                }
+
             }
-            if (player2.Visible == true && p2box.Text == "")
+            
             {
-                
-            }
-            else
-            {
-                
+                MessageBox.Show("je bent iets vergeten");
             }
 
-            if (player3.Visible == true && p3box.Text == "")
-            {
-                
-            }
-            else
-            {
-                p1txt.Text = p1box.Text;
-                p2txt.Text = p2box.Text;
-                p3txt.Text = p3box.Text;
-                p4txt.Text = p4box.Text;
-            }
+
+        }
+
+        private void p1box_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            p1box.BackColor = Color.White;
+        }
+
+        private void p2box_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            p2box.BackColor = Color.White;
+        }
+
+        private void p3box_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            p3box.BackColor = Color.White;
+        }
+
+        private void p4box_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            p4box.BackColor = Color.White;
         }
     }
 }

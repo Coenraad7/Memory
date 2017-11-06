ab5e9980516a50f61deb1e9804eda8c30772084b
”dantheman_0125” <danielroskam@gmail.com>
2017-11-06 16:01:24 +0100

    version 1.67

--- Opdracht/Onderzoeken/MemoryGame/MemoryGame/Endscreen.Designer.cs
+++ Opdracht/Onderzoeken/MemoryGame/MemoryGame/Endscreen.Designer.cs
@@ -28,22 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.place4 = new System.Windows.Forms.Label();
+            this.place3 = new System.Windows.Forms.Label();
+            this.place2 = new System.Windows.Forms.Label();
+            this.place1 = new System.Windows.Forms.Label();
+            this.winlabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.label1.Location = new System.Drawing.Point(495, 86);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(193, 39);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Game over!";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // button1
             // 
@@ -67,25 +59,87 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // label2
+            // place4
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.label2.Location = new System.Drawing.Point(288, 200);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 39);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.place4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.place4.BackColor = System.Drawing.Color.Transparent;
+            this.place4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.place4.ForeColor = System.Drawing.Color.OrangeRed;
+            this.place4.Location = new System.Drawing.Point(12, 470);
+            this.place4.Name = "place4";
+            this.place4.Size = new System.Drawing.Size(1235, 39);
+            this.place4.TabIndex = 12;
+            this.place4.Text = "4";
+            this.place4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.place4.Visible = false;
+            // 
+            // place3
+            // 
+            this.place3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.place3.BackColor = System.Drawing.Color.Transparent;
+            this.place3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.place3.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.place3.Location = new System.Drawing.Point(12, 426);
+            this.place3.Name = "place3";
+            this.place3.Size = new System.Drawing.Size(1235, 46);
+            this.place3.TabIndex = 11;
+            this.place3.Text = "3";
+            this.place3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.place3.Visible = false;
+            // 
+            // place2
+            // 
+            this.place2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.place2.BackColor = System.Drawing.Color.Transparent;
+            this.place2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.place2.ForeColor = System.Drawing.Color.Silver;
+            this.place2.Location = new System.Drawing.Point(12, 369);
+            this.place2.Name = "place2";
+            this.place2.Size = new System.Drawing.Size(1235, 54);
+            this.place2.TabIndex = 10;
+            this.place2.Text = "2";
+            this.place2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.place2.Visible = false;
+            // 
+            // place1
+            // 
+            this.place1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.place1.BackColor = System.Drawing.Color.Transparent;
+            this.place1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.place1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.place1.Location = new System.Drawing.Point(12, 302);
+            this.place1.Name = "place1";
+            this.place1.Size = new System.Drawing.Size(1235, 71);
+            this.place1.TabIndex = 9;
+            this.place1.Text = "1";
+            this.place1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winlabel
+            // 
+            this.winlabel.AutoSize = true;
+            this.winlabel.BackColor = System.Drawing.Color.Transparent;
+            this.winlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold);
+            this.winlabel.ForeColor = System.Drawing.Color.White;
+            this.winlabel.Location = new System.Drawing.Point(514, 61);
+            this.winlabel.Name = "winlabel";
+            this.winlabel.Size = new System.Drawing.Size(235, 54);
+            this.winlabel.TabIndex = 8;
+            this.winlabel.Text = "You Won!";
+            this.winlabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Endscreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MemoryGame.Properties.Resources.Memorygame_players;
             this.ClientSize = new System.Drawing.Size(1259, 676);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.place4);
+            this.Controls.Add(this.place3);
+            this.Controls.Add(this.place2);
+            this.Controls.Add(this.place1);
+            this.Controls.Add(this.winlabel);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Endscreen";
@@ -98,10 +152,12 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label place4;
+        private System.Windows.Forms.Label place3;
+        private System.Windows.Forms.Label place2;
+        private System.Windows.Forms.Label place1;
+        private System.Windows.Forms.Label winlabel;
     }
 }
--- Opdracht/Onderzoeken/MemoryGame/MemoryGame/Endscreen.cs
+++ Opdracht/Onderzoeken/MemoryGame/MemoryGame/Endscreen.cs
@@ -43,21 +43,279 @@ namespace MemoryGame
         private void List()
         {
 
-            //int temp = Math.Max(score1, score2); 
-
-            label2.Text = Variables.playernames[0] + " finished the game with a score of: " + scores[0] + System.Environment.NewLine;
-
+            if (Variables.amountplayers == 1)
+            {
+                place1.Text = "You finished the game with a score of: " + scores[0];
+            }
             if (Variables.amountplayers >= 2)
             {
-                label2.Text += Variables.playernames[1] + " finished the game with a score of: " + scores[1] + System.Environment.NewLine;
+                if (Math.Max(scores[0], scores[1]) == scores[0])
+                {
+                    place1.Text = Variables.playernames[0] + " won with a score of: " + scores[0];
+                    place2.Text = Variables.playernames[1] + " finished with a score of: " + scores[1];
+                }
+                else
+                {
+                    place1.Text = Variables.playernames[1] + " won with a score of: " + scores[1];
+                    place2.Text = Variables.playernames[0] + " finished with a score of: " + scores[0];
+                }
+                place2.Visible = true;
             }
             if (Variables.amountplayers >= 3)
             {
-                label2.Text += Variables.playernames[2] + " finished the game with a score of: " + scores[2] + System.Environment.NewLine;
+                if (Math.Max(scores[0], Math.Max(scores[1], scores[2])) == scores[0])
+                {
+                    place1.Text = Variables.playernames[0] + " won with a score of: " + scores[0];
+
+                    if (Math.Max(scores[1], scores[2]) == scores[1])
+                    {
+                        place2.Text = Variables.playernames[1] + " finished 2nd with a score of: " + scores[1];
+                        place3.Text = Variables.playernames[2] + " finished 3rd with a score of: " + scores[2];
+                    }
+                    else
+                    {
+                        place2.Text = Variables.playernames[2] + " finished 2nd with a score of: " + scores[2];
+                        place3.Text = Variables.playernames[1] + " finished 3rd with a score of: " + scores[1];
+                    }
+                }
+                else if (Math.Max(scores[0], Math.Max(scores[1], scores[2])) == scores[1])
+                {
+                    place1.Text = Variables.playernames[1] + " won with a score of: " + scores[1];
+
+                    if (Math.Max(scores[0], scores[2]) == scores[0])
+                    {
+                        place2.Text = Variables.playernames[0] + " finished 2nd with a score of: " + scores[0];
+                        place3.Text = Variables.playernames[2] + " finished 3rd with a score of: " + scores[2];
+                    }
+                    else
+                    {
+                        place2.Text = Variables.playernames[2] + " finished 2nd with a score of: " + scores[2];
+                        place3.Text = Variables.playernames[0] + " finished 3rd with a score of: " + scores[0];
+                    }
+                }
+                else
+                {
+                    place1.Text = Variables.playernames[2] + " won with a score of: " + scores[2];
+
+                    if (Math.Max(scores[1], scores[2]) == scores[1])
+                    {
+                        place2.Text = Variables.playernames[1] + " finished 2nd with a score of: " + scores[1];
+                        place3.Text = Variables.playernames[2] + " finished 3rd with a score of: " + scores[2];
+                    }
+                    else
+                    {
+                        place2.Text = Variables.playernames[2] + " finished 2nd with a score of: " + scores[2];
+                        place3.Text = Variables.playernames[1] + " finished 3rd with a score of: " + scores[1];
+                    }
+
+                }
+
+                place3.Visible = true;
             }
+
             if (Variables.amountplayers == 4)
             {
-                label2.Text += Variables.playernames[3] + " finished the game with a score of: " + scores[3] + System.Environment.NewLine;
+                if (Math.Max(scores[0], Math.Max(scores[1], Math.Max(scores[2], scores[3]))) == scores[0])
+                {
+                    #region als player 1 wint
+                    place1.Text = Variables.playernames[0] + " won with a score of: " + scores[0];
+                    winlabel.Text = Variables.playernames[0] + " won";
+
+                    if (Math.Max(scores[1], Math.Max(scores[2], scores[3])) == scores[1])
+                    {
+                        place2.Text = Variables.playernames[1] + " finished 2nd with a score of: " + scores[1];
+                        if (Math.Max(scores[2], scores[3]) == scores[2])
+                        {
+                            place3.Text = Variables.playernames[2] + " finished 3rd with a score of: " + scores[2];
+                            place4.Text = Variables.playernames[3] + " finished 4th with a score of: " + scores[3];
+                        }
+                        else
+                        {
+                            place3.Text = Variables.playernames[2] + " finished 3rd with a score of: " + scores[3];
+                            place4.Text = Variables.playernames[3] + " finished 4th with a score of: " + scores[2];
+                        }
+                    }
+                    else if (Math.Max(scores[1], Math.Max(scores[2], scores[3])) == scores[2])
+                    {
+                        place2.Text = Variables.playernames[2] + " finished 2nd with a score of: " + scores[2];
+                        if (Math.Max(scores[1], scores[3]) == scores[1])
+                        {
+                            place3.Text = Variables.playernames[1] + " finished 3rd with a score of: " + scores[1];
+                            place4.Text = Variables.playernames[3] + " finished 4th with a score of: " + scores[3];
+                        }
+                        else
+                        {
+                            place3.Text = Variables.playernames[3] + " finished 3rd with a score of: " + scores[3];
+                            place4.Text = Variables.playernames[1] + " finished 4th with a score of: " + scores[1];
+                        }
+                    }
+                    else
+                    {
+                        place2.Text = Variables.playernames[3] + " finished 2nd with a score of: " + scores[3];
+                        if (Math.Max(scores[1], scores[2]) == scores[1])
+                        {
+                            place3.Text = Variables.playernames[1] + " finished 3rd with a score of: " + scores[1];
+                            place4.Text = Variables.playernames[2] + " finished 4th with a score of: " + scores[2];
+                        }
+                        else
+                        {
+                            place3.Text = Variables.playernames[2] + " finished 3rd with a score of: " + scores[2];
+                            place4.Text = Variables.playernames[1] + " finished 4th with a score of: " + scores[1];
+                        }
+                    }
+                }
+                #endregion
+                else if (Math.Max(scores[0], Math.Max(scores[1], Math.Max(scores[2], scores[3]))) == scores[1])
+                {
+                    #region als player 2 wint;
+                    place1.Text = Variables.playernames[1] + " won with a score of: " + scores[1];
+                    winlabel.Text = Variables.playernames[1] + " won";
+
+                    if (Math.Max(scores[0], Math.Max(scores[2], scores[3])) == scores[0])
+                    {
+                        place2.Text = Variables.playernames[0] + " finished 2nd with a score of: " + scores[0];
+                        if (Math.Max(scores[2], scores[3]) == scores[2])
+                        {
+                            place3.Text = Variables.playernames[2] + " finished 3rd with a score of: " + scores[2];
+                            place4.Text = Variables.playernames[3] + " finished 4th with a score of: " + scores[3];
+                        }
+                        else
+                        {
+                            place3.Text = Variables.playernames[3] + " finished 3rd with a score of: " + scores[3];
+                            place4.Text = Variables.playernames[2] + " finished 4th with a score of: " + scores[2];
+                        }
+                    }
+                    else if (Math.Max(scores[0], Math.Max(scores[2], scores[3])) == scores[2])
+                    {
+                        place2.Text = Variables.playernames[2] + " finished 2nd with a score of: " + scores[2];
+                        if (Math.Max(scores[0], scores[3]) == scores[0])
+                        {
+                            place3.Text = Variables.playernames[0] + " finished 3rd with a score of: " + scores[0];
+                            place4.Text = Variables.playernames[3] + " finished 4th with a score of: " + scores[3];
+                        }
+                        else
+                        {
+                            place3.Text = Variables.playernames[3] + " finished 3rd with a score of: " + scores[3];
+                            place4.Text = Variables.playernames[0] + " finished 4th with a score of: " + scores[0];
+                        }
+                    }
+                    else
+                    {
+                        place2.Text = Variables.playernames[3] + " finished 2nd with a score of: " + scores[3];
+                        if (Math.Max(scores[0], scores[2]) == scores[0])
+                        {
+                            place3.Text = Variables.playernames[0] + " finished 3rd with a score of: " + scores[0];
+                            place4.Text = Variables.playernames[2] + " finished 4th with a score of: " + scores[2];
+                        }
+                        else
+                        {
+                            place3.Text = Variables.playernames[2] + " finished 3rd with a score of: " + scores[2];
+                            place4.Text = Variables.playernames[0] + " finished 4th with a score of: " + scores[0];
+                        }
+                    }
+                }
+                #endregion
+                else if (Math.Max(scores[0], Math.Max(scores[1], Math.Max(scores[2], scores[3]))) == scores[1])
+                {
+                    #region als player 3 wint;
+                    place1.Text = Variables.playernames[2] + " won with a score of: " + scores[2];
+                    winlabel.Text = Variables.playernames[2] + " won";
+
+                    if (Math.Max(scores[0], Math.Max(scores[1], scores[3])) == scores[0])
+                    {
+                        place1.Text = Variables.playernames[0] + " finished 2nd with a score of: " + scores[0];
+                        if (Math.Max(scores[1], scores[3]) == scores[1])
+                        {
+                            place3.Text = Variables.playernames[1] + " finished 3rd with a score of: " + scores[1];
+                            place4.Text = Variables.playernames[3] + " finished 4th with a score of: " + scores[3];
+                        }
+                        else
+                        {
+                            place3.Text = Variables.playernames[3] + " finished 3rd with a score of: " + scores[3];
+                            place4.Text = Variables.playernames[1] + " finished 4th with a score of: " + scores[1];
+                        }
+                    }
+                    else if (Math.Max(scores[0], Math.Max(scores[1], scores[3])) == scores[1])
+                    {
+                        place1.Text = Variables.playernames[1] + " finished 2nd with a score of: " + scores[1];
+                        if (Math.Max(scores[0], scores[3]) == scores[0])
+                        {
+                            place3.Text = Variables.playernames[0] + " finished 3rd with a score of: " + scores[0];
+                            place4.Text = Variables.playernames[3] + " finished 4th with a score of: " + scores[3];
+                        }
+                        else
+                        {
+                            place3.Text = Variables.playernames[3] + " finished 3rd with a score of: " + scores[3];
+                            place4.Text = Variables.playernames[0] + " finished 4th with a score of: " + scores[0];
+                        }
+                    }
+                    else
+                    {
+                        place1.Text = Variables.playernames[3] + " finished 2nd with a score of: " + scores[3];
+                        if (Math.Max(scores[0], scores[1]) == scores[0])
+                        {
+                            place3.Text = Variables.playernames[0] + " finished 3rd with a score of: " + scores[0];
+                            place4.Text = Variables.playernames[1] + " finished 4th with a score of: " + scores[1];
+                        }
+                        else
+                        {
+                            place3.Text = Variables.playernames[1] + " finished 3rd with a score of: " + scores[1];
+                            place4.Text = Variables.playernames[0] + " finished 4th with a score of: " + scores[0];
+                        }
+                    }
+                    #endregion
+                }
+                else
+                {
+                    #region als player 4 wint;
+                    place1.Text = Variables.playernames[3] + " won with a score of: " + scores[3];
+                    winlabel.Text = Variables.playernames[3] + " won";
+
+                    if (Math.Max(scores[0], Math.Max(scores[1], scores[2])) == scores[0])
+                    {
+                        place1.Text = Variables.playernames[0] + " finished 2nd with a score of: " + scores[0];
+                        if (Math.Max(scores[1], scores[2]) == scores[1])
+                        {
+                            place2.Text = Variables.playernames[1] + " finished 3rd with a score of: " + scores[1];
+                            place4.Text = Variables.playernames[2] + " finished 4th with a score of: " + scores[2];
+                        }
+                        else
+                        {
+                            place2.Text = Variables.playernames[2] + " finished 3rd with a score of: " + scores[2];
+                            place4.Text = Variables.playernames[1] + " finished 4th with a score of: " + scores[1];
+                        }
+                    }
+                    else if (Math.Max(scores[0], Math.Max(scores[1], scores[2])) == scores[1])
+                    {
+                        place1.Text = Variables.playernames[1] + " finished 2nd with a score of: " + scores[1];
+                        if (Math.Max(scores[0], scores[2]) == scores[0])
+                        {
+                            place2.Text = Variables.playernames[0] + " finished 3rd with a score of: " + scores[0];
+                            place4.Text = Variables.playernames[2] + " finished 4th with a score of: " + scores[2];
+                        }
+                        else
+                        {
+                            place2.Text = Variables.playernames[2] + " finished 3rd with a score of: " + scores[2];
+                            place4.Text = Variables.playernames[0] + " finished 4th with a score of: " + scores[0];
+                        }
+                    }
+                    else
+                    {
+                        place1.Text = Variables.playernames[2] + " finished 2nd with a score of: " + scores[2];
+                        if (Math.Max(scores[0], scores[1]) == scores[0])
+                        {
+                            place2.Text = Variables.playernames[0] + " finished 3rd with a score of: " + scores[0];
+                            place4.Text = Variables.playernames[1] + " finished 4th with a score of: " + scores[1];
+                        }
+                        else
+                        {
+                            place2.Text = Variables.playernames[1] + " finished 3rd with a score of: " + scores[1];
+                            place4.Text = Variables.playernames[0] + " finished 4th with a score of: " + scores[0];
+                        }
+                    }
+                    #endregion
+                }
+                place4.Visible = true;
             }
         }
     }

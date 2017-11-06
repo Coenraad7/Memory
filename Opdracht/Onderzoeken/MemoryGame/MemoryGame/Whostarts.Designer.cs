0798565d2fe5aefcd731cb91c8c95b3069fdcd72
Berry Kooistra 1G <kooi1722@student.nhl.nl>
2017-11-06 09:46:43 +0100

    Version 1.60

--- Opdracht/Onderzoeken/MemoryGame/MemoryGame/Whostarts.Designer.cs
+++ Opdracht/Onderzoeken/MemoryGame/MemoryGame/Whostarts.Designer.cs
@@ -29,11 +29,10 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Whostarts));
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Meeseeks = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Meeseeks)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -50,31 +49,32 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // pictureBox1
+            // Meeseeks
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(484, 24);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(250, 163);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.Meeseeks.Image = global::MemoryGame.Properties.Resources.Mr_Meeseeks;
+            this.Meeseeks.Location = new System.Drawing.Point(494, -9);
+            this.Meeseeks.Name = "Meeseeks";
+            this.Meeseeks.Size = new System.Drawing.Size(225, 299);
+            this.Meeseeks.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Meeseeks.TabIndex = 1;
+            this.Meeseeks.TabStop = false;
+            this.Meeseeks.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Whostarts
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1259, 676);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Meeseeks);
             this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Whostarts";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Meeseeks)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +82,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.PictureBox Meeseeks;
     }
 }

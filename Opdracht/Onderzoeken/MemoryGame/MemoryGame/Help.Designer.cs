9faf4fcd67c00eaa41624dcee9556b061ddb6158
dantheman0125 <danielroskam@gmail.com>
2017-11-07 10:40:35 +0100

    version 1.69

--- Opdracht/Onderzoeken/MemoryGame/MemoryGame/Help.Designer.cs
+++ Opdracht/Onderzoeken/MemoryGame/MemoryGame/Help.Designer.cs
@@ -30,12 +30,13 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.button1.Location = new System.Drawing.Point(1016, 613);
+            this.button1.Location = new System.Drawing.Point(31, 613);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(231, 51);
             this.button1.TabIndex = 19;
@@ -65,36 +66,67 @@
             "",
             "WHAT IS THE GOAL?:",
             "",
-            "The object of the game is to collect the most matching pairs.",
+            "Singleplayer: \tWhen you play by yourself the object of the game is to get as much" +
+                " points as possible.",
+            "Multiplayer: \tThe object of the game is to collect the most matching pairs.",
             "",
             "HOW TO PLAY?:",
             "",
-            "The first player chooses a random card. The same player then selects another rand" +
-                "om card. If the two cards are excactly the same they get a point and choose ",
-            "again 2 random cards. If the cards are not a match your turn is over and it is no" +
-                "w the next players turn. The next player chooses their 2 random card. If they ar" +
-                "e ",
-            "excactly the same they get a point and choose again 2 cards. The game continues i" +
-                "n this fashion until all the cards are played.",
+            "Singleplayer:\tWhen you start the game, you click on 2 random cards. If the 2 card" +
+                "s are excactly the same, you will get a point and the cards will disappear. If t" +
+                "hey are not, ",
+            "\t\tthe cards will flip and you may try again. The game continues in this fashion u" +
+                "ntil the board is empty.",
+            "\t\tYou can check the highest score in the \"Highscore\"-window. Your",
+            "",
+            "Multiplayer: \tThe first player chooses a random card. The same player then select" +
+                "s another random card. If the two cards are excactly the same they get a point a" +
+                "nd choose ",
+            "\t\tagain 2 random cards. If the cards are not a match your turn is over and it is " +
+                "now the next players turn. The next player chooses their 2 random card. If they " +
+                "are ",
+            "\t\texcactly the same they get a point and choose again 2 cards. The game continues" +
+                " in this fashion until all the cards are played.",
             "",
             "HOW DO I WIN?:",
+            "",
             "Once all the cards have been played the player with the most matching pairs is th" +
                 "e winner.",
             "",
+            "",
+            "",
+            "",
+            "",
             "HINT:",
-            "Just because it is another players turn doesn\'t mean you should stop paying atten" +
-                "tion. The cards that other players flip over could be the match you\'re looking f" +
-                "or."});
-            this.listBox1.Location = new System.Drawing.Point(12, 53);
+            "",
+            "Singleplayer:\tWhen you get multiple pairs in a row.",
+            "Multiplayer: \tJust because it is another players turn doesn\'t mean you should sto" +
+                "p paying attention. The cards that other players flip over could be the match yo" +
+                "u\'re ",
+            "\t\tlooking for. ",
+            "\t\tDistract your opponent when it is their turn. When the timer is back to 0 it is" +
+                " your turn again."});
+            this.listBox1.Location = new System.Drawing.Point(12, 72);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(1235, 498);
             this.listBox1.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 39);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Help";
             // 
             // Help
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1259, 676);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.DoubleBuffered = true;
@@ -103,11 +135,13 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Help";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Opdracht/Onderzoeken/MemoryGame/MemoryGame/Highscores.Designer.cs
+++ Opdracht/Onderzoeken/MemoryGame/MemoryGame/Highscores.Designer.cs
@@ -42,7 +42,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
-            this.button1.Location = new System.Drawing.Point(1009, 613);
+            this.button1.Location = new System.Drawing.Point(31, 613);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(238, 51);
             this.button1.TabIndex = 20;
